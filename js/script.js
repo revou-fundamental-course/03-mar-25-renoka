9218dd3b3304452b2459ca493dd919cc9802e687
Renoka Tresna Pramuda <samsungrenoka@gmail.com>
2025-03-13 08:16:33 +0700

    create index

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// function replacename(){
+// let nama = prompt('Halo, siapa nama anda','')
+
+// document.getElementById("nama").innerHTML = nama
+// }
+// replacename()
+
+// document.getElementById('klik').addEventListener('click', function(){replacename()})
+
